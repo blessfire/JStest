f9008e8e977bc8250a3c494c42b31618fbb1577b
Volodymyr Ionaytis <blessfire91@gmail.com>
2023-05-17 09:23:38 +0200

    HW

---
+++ js/test.js
@@ -0,0 +1,96 @@
+const 
+    
+
+// const productName = "Droid";
+// const pricePerItem = 3500;
+// // Change code below this line
+// const message = `You picked ${productName}, price per item is ${pricePerItem} credits`;
+// console.log(message);
+
+// const pricePerItem = 3500;
+// const orderedQuantity = 4;
+
+// // Change code below this line
+
+// const totalPrice = pricePerItem * orderedQuantity;
+// console.log(totalPrice)
+
+// const topSpeed = 160;
+// const distance = 617.54;
+// const login = "mango935";
+// const isOnline = true;
+// const isAdmin = false;
+
+// // Числа
+// const age = 20;
+// const salary = 3710.84;
+
+// // Рядки
+// const name = "Mango";
+// const description = "JavaSript essentials";
+
+// // Булі
+// const isModalOpen = true;
+// const isLoggedIn = false;
+
+// let productName = "Droid";
+// let pricePerItem = 2000;
+// productName = "Repair droid";
+// pricePerItem = pricePerItem + 1500;
+
+// // // Change code below this line
+// // const productName = "Droid";
+// // const pricePerItem = "2000";
+// // // console.log(productName);
+// // // 'Droid'
+// console.log(productName);
+// // // console.log(pricePerItem);
+// // // 2000
+pricePerDroid - ціна одного дроїда, значення 800
+orderedQuantity - кількість дроїдів у замовленні, значення 6
+deliveryFee - вартість доставки, значення 50
+totalPrice - загальна сума замовлення до сплати, не забудь про вартість доставки
+message - повідомлення про стан замовлення у форматі "You ordered droids worth <total price> credits. Delivery (<delivery fee> credits) is included in total price."
+Оголошена змінна orderedQuantity
+Значення змінної orderedQuantity - це число 6
+Оголошена змінна pricePerDroid
+Значення змінної pricePerDroid - це число 800
+Оголошена змінна deliveryFee
+Значення змінної deliveryFee - це число 50
+Оголошена змінна totalPrice
+Значення змінної totalPrice - це число 4850
+Оголошена змінна message
+Значення змінної message - це рядок "You ordered droids worth 4850 credits. Delivery (50 credits) is included in total price."
+// Оголошена змінна productName
+// Значення змінної productName - це рядок "Droid"
+// Оголошена змінна pricePerItem
+// Значення змінної price - це число 3500
+// Оголошена змінна message
+// Значення змінної message - це рядок "You picked Droid, price per item is 3500 credits"
+// const guestName = "Mango";
+// const roomNumber = 207;
+// const greeting = `Ласкаво просимо ${guestName}, ви заселені у номер ${roomNumber}`;
+// Оголошена змінна pricePerItem
+// Значення змінної pricePerItem - це число 3500
+// Оголошена змінна orderedQuantity
+// Значення змінної orderedQuantity - це число 4
+// Оголошена змінна totalPrice
+// Значення змінної totalPrice - це число 14000
+// Використаний оператор *
+
+// console.log(Number(pricePerItem));
+// topSpeed - число 160.
+// distance - число 617.54.
+// login - рядок "mango935".
+// isOnline - буль true.
+// isAdmin - буль false.
+// Оголошена змінна topSpeed
+// Значення змінної topSpeed - це число 160
+// Оголошена змінна distance
+// Значення змінної distance - це число 617.54
+// Оголошена змінна login
+// Значення змінної login - це рядок "mango935"
+// Оголошена змінна isOnline
+// Значення змінної isOnline - це буль true
+// Оголошена змінна isAdmin
+// Значення змінної isAdmin - це буль false
